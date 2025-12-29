7baf0b4c89eb269a2ee8b37adf8efdad3f11e619
Yuvraj Prasad <yuvrajprasad1590@gmail.com>
2025-12-29 18:41:53 +0530

    styling in both and ejs concept in 12

---
+++ lec-11-airbnb-styling/app.js
@@ -0,0 +1,29 @@
+// Core Module
+const path = require('path');
+
+// External Module
+const express = require('express');
+
+//Local Module
+const userRouter = require("./routes/userRouter")
+const hostRouter = require("./routes/hostRouter")
+const rootDir = require("./utils/pathUtil");
+
+const app = express();
+
+// 1. Parse incoming bodies
+app.use(express.urlencoded({ extended: true }));
+
+// 2. Serve static files (Crucial for your output.css)
+app.use(express.static(path.join(rootDir, 'public')));
+app.use(userRouter);
+app.use("/host", hostRouter);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
